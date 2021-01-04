1af03b8ade0cc18f1d8996d804d120a8faef3bb0
Heyward Fann <fannheyward@gmail.com>
2021-01-04 16:58:25 +0800

    Revert "feat(language-client): client.end"
    This reverts commit 5b7137fb50dace3d23b9aae37bbb376986f3f57a.

--- src/language-client/client.ts
+++ src/language-client/client.ts
@@ -92,7 +92,6 @@ interface IConnection {
   didSaveTextDocument(params: DidSaveTextDocumentParams): void
   onDiagnostics(handler: NotificationHandler<PublishDiagnosticsParams>): void
 
-  end(): void;
   dispose(): void
 }
 
@@ -234,7 +233,6 @@ function createConnection(
     onDiagnostics: (handler: NotificationHandler<PublishDiagnosticsParams>) =>
       connection.onNotification(PublishDiagnosticsNotification.type, handler),
 
-    end: () => connection.end(),
     dispose: () => connection.dispose()
   }
 
@@ -3779,7 +3777,6 @@ export abstract class BaseLanguageClient {
     return (this._onStop = this.resolveConnection().then(connection => {
       return connection.shutdown().then(() => {
         connection.exit()
-        connection.end()
         connection.dispose()
         this.state = ClientState.Stopped
         this.cleanUpChannel()
