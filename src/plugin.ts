b07bc8c8e9cf37f1e79e3771723f374d4638b7fa
Heyward Fann <fannheyward@gmail.com>
2021-01-07 16:51:10 +0800

    feat(handler): add getSemanticTokens
    :call CocAction('semanticTokens') works

--- src/plugin.ts
+++ src/plugin.ts
@@ -386,6 +386,9 @@ export default class Plugin extends EventEmitter {
     this.addAction('callHierarchy', () => {
       return this.handler.getCallHierarchy()
     })
+    this.addAction('semanticTokens', () => {
+      return this.handler.getSemanticTokens()
+    })
     commandManager.init(nvim, this)
   }
 
